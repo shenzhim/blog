16a76f3768d8f8085187aa1c7510479b14154da7
shenzhimin <zhimin.shen@yoho.cn>
2016-11-08 15:00:34 +0800

    框架搭建

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -8,12 +8,11 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// handle fallback for HTML5 history API
+app.use(require('connect-history-api-fallback')())
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
