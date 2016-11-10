8922139e40cbbaee882e41c49b7ba35596fb863b
shenzhimin <zhimin.shen@yoho.cn>
2016-11-10 14:53:54 +0800

    no message

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -9,6 +9,13 @@ var bodyParser = require('body-parser');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(cookieParser());
+
 app.use(require('connect-history-api-fallback')())
 if (app.get('env') === 'development') {
 	var compiler = webpack(require('./public/webpack.config'))
@@ -20,22 +27,15 @@ if (app.get('env') === 'development') {
 		}
 	})
 	app.use(devMiddleware);
+} else {
+	app.use(express.static(path.join(__dirname, 'public/dist')));
 }
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/dist')));
 app.use(compression());
 
-// dispatcher
 try {
 	require('./dispatch')(app);
 
-	// catch 404 and forward to error handler
 	app.use(function(req, res, next) {
 		var err = new Error('Not Found');
 		err.status = 404;
@@ -44,11 +44,8 @@ try {
 
 	// error handler
 	app.use(function(err, req, res, next) {
-		// set locals, only providing error in development
 		res.locals.message = err.message;
 		res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-		// render the error page
 		res.status(err.status || 500);
 		res.render('error');
 	});
@@ -56,7 +53,6 @@ try {
 	console.error(err);
 }
 
-// listener
 app.listen(app.get('port'), function() {
 	console.log('blog server start on port: ', app.get('port'));
 });
