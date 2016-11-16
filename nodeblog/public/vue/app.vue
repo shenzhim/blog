f95d851f5dc0135be74709426b81626bf6d1a3b7
shenzhimin <zhimin.shen@yoho.cn>
2016-11-16 18:39:24 +0800

    更换图标

--- nodeblog/public/vue/app.vue
+++ nodeblog/public/vue/app.vue
@@ -13,11 +13,11 @@
         <h1 class="title">Shenzm.志敏</h1>
         <p class="describe">一个男人总要有一技之长来养家糊口...</p>
         <ul class="navcontainer">
-          <li class="nav_1"><router-link class="" to="/foo"><svg class="icon icon-user"><use xlink:href="#icon-user"></use></svg></router-link></li>
-          <li class="nav_2"><router-link class="" to="/foo"><svg class="icon icon-paragraph-left"><use xlink:href="#icon-paragraph-left"></use></svg></router-link></li>
+          <li class="nav_1"><router-link class="" to="/foo"><svg class="icon icon-smile-o"><use xlink:href="#icon-smile-o"></use></svg></router-link></li>
+          <li class="nav_2"><router-link class="" to="/foo"><svg class="icon icon-th"><use xlink:href="#icon-th"></use></svg></router-link></li>
           <li class="nav_3"><router-link class="" to="/foo"><svg class="icon icon-github"><use xlink:href="#icon-github"></use></svg></router-link></li>
-          <li class="nav_4"><router-link class="" to="/foo"><svg class="icon icon-mail"><use xlink:href="#icon-mail"></use></svg></router-link></li>
-          <li class="nav_5"><router-link class="" to="/foo"><svg class="icon icon-rss"><use xlink:href="#icon-rss"></use></svg></router-link></li>
+          <li class="nav_4"><router-link class="" to="/foo"><svg class="icon icon-envelope-square"><use xlink:href="#icon-envelope-square"></use></svg></router-link></li>
+          <li class="nav_5"><router-link class="" to="/foo"><svg class="icon icon-feed"><use xlink:href="#icon-feed"></use></svg></router-link></li>
         </ul>
     </div>
     <footer class="footer">
