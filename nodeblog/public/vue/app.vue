ac30274cdc703c18bcfa91fcac8f8021a219f627
shenzhimin <zhimin.shen@yoho.cn>
2016-11-16 15:46:46 +0800

    修改导航栏样式

--- nodeblog/public/vue/app.vue
+++ nodeblog/public/vue/app.vue
@@ -10,14 +10,14 @@
     <div class="overlay"></div>
     <div class="content">
         <a class="avatar" href="#"><img src="../img/avatar.png" alt="shenzm"></a>
-        <h1>Shenzm.志敏</h1>
-        <p>一个男人总要有一技之长来养家糊口...</p>
-        <ul>
-          <li><router-link class="" to="/foo"><svg class="icon icon-user"><use xlink:href="#icon-user"></use></svg></router-link></li>
-          <li><router-link class="" to="/foo"><svg class="icon icon-user"><use xlink:href="#icon-paragraph-left"></use></svg></router-link></li>
-          <li><router-link class="" to="/foo"><svg class="icon icon-user"><use xlink:href="#icon-github"></use></svg></router-link></li>
-          <li><router-link class="" to="/foo"><svg class="icon icon-user"><use xlink:href="#icon-mail"></use></svg></router-link></li>
-          <li><router-link class="" to="/foo"><svg class="icon icon-user"><use xlink:href="#icon-rss"></use></svg></router-link></li>
+        <h1 class="title">Shenzm.志敏</h1>
+        <p class="describe">一个男人总要有一技之长来养家糊口...</p>
+        <ul class="navcontainer">
+          <li class="nav_1"><router-link class="" to="/foo"><svg class="icon icon-user"><use xlink:href="#icon-user"></use></svg></router-link></li>
+          <li class="nav_2"><router-link class="" to="/foo"><svg class="icon icon-paragraph-left"><use xlink:href="#icon-paragraph-left"></use></svg></router-link></li>
+          <li class="nav_3"><router-link class="" to="/foo"><svg class="icon icon-github"><use xlink:href="#icon-github"></use></svg></router-link></li>
+          <li class="nav_4"><router-link class="" to="/foo"><svg class="icon icon-mail"><use xlink:href="#icon-mail"></use></svg></router-link></li>
+          <li class="nav_5"><router-link class="" to="/foo"><svg class="icon icon-rss"><use xlink:href="#icon-rss"></use></svg></router-link></li>
         </ul>
     </div>
     <footer class="footer">
