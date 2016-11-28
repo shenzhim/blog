02a82150f26e85744575c666b869ab2eef3409e5
shenzhimin <zhimin.shen@yoho.cn>
2016-11-28 22:46:14 +0800

    blog列表布局

--- nodeblog/apps/blog/model.js
+++ nodeblog/apps/blog/model.js
@@ -1,16 +1,18 @@
 const dbs = require('../../dbs');
+const date = require("../../utils/date");
 
 module.exports = {
 	getList: function() {
 		return dbs.data.list(1, 'blog').then(function(result) {
-			var list = [];
+			const list = [];
 			if (result) {
 				result.forEach(function(o) {
 					if (o.sort > 0) {
 						list.push({
 							title: JSON.parse(o.value).title,
 							msgid: o.bindid,
-							tag: o.tag
+							tag: o.tag.split(' · '),
+							time: date.format(new Date(o.created), "yyyy年 MM月 dd日")
 						});
 					}
 				})
--- nodeblog/public/vue/blog/list.vue
+++ nodeblog/public/vue/blog/list.vue
@@ -4,8 +4,14 @@
 			<ul>
 				<li v-for="item in list">
 				    <router-link :to="'/' + item.msgid">
-				    	<img src="">
-				    	<span>{{item.title}}</span>
+				    	<img src="http://t.named.cn/f/e20cbd14c8a0fb2e56e81b31b787812a.t358x236.fa.r8.b1x1x3x1xf3f3f3.lfbfbfb.jpg">
+				    	<div class="blog-title">
+				    		<h4 class="title">{{item.title}}</h4>
+				    		<div class="info">
+				    			<span v-for="tag in item.tag" class="tag">{{tag}}</span>
+				    			<span class="time">{{item.time}}</span>	
+				    		</div>
+				    	</div>
 				    </router-link>
 				</li>
 			</ul>
