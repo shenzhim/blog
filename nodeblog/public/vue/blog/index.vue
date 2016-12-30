ec92d5b4d5c6dd3d596c01b0e0863feb5fa6e348
shenzhimin <zhimin.shen@yoho.cn>
2016-12-30 15:37:48 +0800

    修改bug

--- nodeblog/public/vue/blog/index.vue
+++ nodeblog/public/vue/blog/index.vue
@@ -3,12 +3,12 @@
 		<div class="blog">
 		    <div class="menubar">
 		    	<ul>
-		    		<li><img src="../../img/avatar.png" alt="shenzm"></li>
+		    		<li><router-link to="/"><img src="../../img/avatar.png" alt="shenzm"></router-link></li>
 		    		<li><router-link to="/blog/me"><svg class="icon icon-smile-o"><use xlink:href="#icon-smile-o"></use></svg></router-link></li>
             		<li><router-link to="/blog/list"><svg class="icon icon-align-left"><use xlink:href="#icon-align-left"></use></svg></router-link></li>
             		<li><a href="https://github.com/shenzhim" target="_blank"><svg class="icon icon-github"><use xlink:href="#icon-github"></use></svg></a></li>
             		<li><a href="mailto:szmalq@163.com"><svg class="icon icon-envelope-square"><use xlink:href="#icon-envelope-square"></use></svg></a></li>
-            		<li><a href="https://github.com/shenzhim" target="_blank"><svg class="icon icon-feed"><use xlink:href="#icon-feed"></use></svg></a></li>
+            		<li><a href="/rss" target="_blank"><svg class="icon icon-feed"><use xlink:href="#icon-feed"></use></svg></a></li>
 		    	</ul>
 		    </div>
 		    <div class="container">
