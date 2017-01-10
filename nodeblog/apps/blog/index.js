3acc5a5853aa93185fe03c352cebf34e5911b5c6
shenzhimin <zhimin.shen@yoho.cn>
2017-01-10 11:53:12 +0800

    增加 connect-multiparty

--- nodeblog/apps/blog/index.js
+++ nodeblog/apps/blog/index.js
@@ -1,5 +1,7 @@
 var app = require('express')();
 var model = require('./model');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 app.get('/list', function(req, res, next) {
 	model.getList().then(function(data) {
@@ -7,7 +9,8 @@ app.get('/list', function(req, res, next) {
 	}).catch(next)
 });
 
-app.post('/postimg', function(req, res, next) {
+app.post('/postimg', multipartMiddleware, function(req, res, next) {
+	console.log(req.body, req.files);
 	res.end("{'code':'1','id':'imgID','src':'test.jpg'}")
 });
 
