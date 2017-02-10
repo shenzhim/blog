c7b736650292a2b550b25334621dd16ab17f7a40
shenzhimin <zhimin.shen@yoho.cn>
2017-02-10 15:56:09 +0800

    back to top 增加动画

--- nodeblog/public/vue/blog/message.vue
+++ nodeblog/public/vue/blog/message.vue
@@ -111,7 +111,29 @@ export default {
 	},
 	methods: {
 		gotop() {
-			window.scroll(0, 0);
+			var doc = document.body.scrollTop? document.body : document.documentElement;
+			this.easeout(doc.scrollTop, 0, 4, function (value) {
+    			doc.scrollTop = value;
+			});
+		},
+		easeout(A, B, rate, callback) {
+			if (A == B || typeof A != 'number') {
+        		return;    
+    		}
+    		B = B || 0;
+    		rate = rate || 2;
+    
+    		var step = function () {
+        		A = A + (B - A) / rate;
+        
+        		if (A < 1) {
+            		callback(B, true);
+            		return;
+        		}
+        		callback(A, false);
+        		requestAnimationFrame(step);    
+    		};
+    		step();
 		}
 	}
 }
